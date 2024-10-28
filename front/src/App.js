5d87888a207ac3100221de18b92507dacc012ae5
cm <mingyeong775@naver.com>
2024-10-28 15:27:47 +0900

    f_main

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,20 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Main from './components/main/main';
+import Notification from './components/notification/notification';
+import Login from './components/auth/login';
 
-function App() {
-  return <div className='App'></div>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/notification" element={<Notification />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ front/src/components/main/main.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+import './main.css';
+import Video from '../video/redcar.mp4';
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      {/* header */}
+      <header className="header">
+        <Header />
+      </header>
+
+      {/* Video Section */}
+      <section className="video-section">
+        <video className="background-video" autoPlay loop muted>
+          <source src={Video} type="video/mp4" />
+        </video>
+      </section>
+
+      {/* footer */}
+      <div className="footer">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Main;
