d125f456835c3670e4f57d05bf7374e268dd1893
cm <mingyeong775@naver.com>
2024-10-28 16:47:20 +0900

    f_footer_font

--- front/src/components/footer/footer.js
+++ front/src/components/footer/footer.js
@@ -5,8 +5,10 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-logo">CARBOT</div>
-      <div className="footer-copyright">©2024 Carbot. All Rights Reserved.</div>
-      <div className="footer-links">
+      <div className="footer-copyright">
+        ©2024 <span className="logo-colored">Carbot</span>. All Rights Reserved.
+      </div>
+      <div className="footer-text">
         개인정보 처리방침 | 이용약관 | 저작권 안내
       </div>
     </footer>
