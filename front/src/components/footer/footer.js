da6b67cddd231f48e1cb49994db9f4efd2f59874
cm <mingyeong775@naver.com>
2024-10-28 15:27:31 +0900

    f_footer

---
+++ front/src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-logo">CARBOT</div>
+      <div className="footer-copyright">©2024 Carbot. All Rights Reserved.</div>
+      <div className="footer-links">
+        개인정보 처리방침 | 이용약관 | 저작권 안내
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
