bba1d0138b480baa427833df4b5793fc26829fa9
cm <mingyeong775@naver.com>
2024-10-30 10:59:54 +0900

    f_mypage_iniit

---
+++ front/src/components/mypage/mypage.js
@@ -0,0 +1,99 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Eximage from '../../images/genesis.png'; // 이미지 경로 설정
+import './mypage.css';
+import Proposal from '../mypage/modal/proposal';
+
+function MyPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [activePage, setActivePage] = useState('interest');
+
+  const renderContent = () => {
+    switch (activePage) {
+      case 'interest':
+        return (
+          <div className="container">
+            <div className="card">
+              <img src={Eximage} alt="GV80 coupe" />
+              <div className="info">
+                <p>모델명: GV80 coupe</p>
+                <div className="proposal-button">
+                  <button onClick={() => setIsModalOpen(true)}>
+                    구매 상담 신청
+                  </button>
+                </div>
+
+                {isModalOpen && (
+                  <Proposal onClose={() => setIsModalOpen(false)} />
+                )}
+              </div>
+            </div>
+            <div className="card">
+              <img src={Eximage} alt="GV80 coupe" />
+              <div className="info">
+                <p>모델명: GV80 coupe</p>
+                <div className="proposal-button">
+                  <button onClick={() => setIsModalOpen(true)}>
+                    구매 상담 신청
+                  </button>
+                </div>
+
+                {isModalOpen && (
+                  <Proposal onClose={() => setIsModalOpen(false)} />
+                )}
+              </div>
+            </div>
+            <div className="card">
+              <img src={Eximage} alt="GV80 coupe" />
+              <div className="info">
+                <p>모델명: GV80 coupe</p>
+                <div className="proposal-button">
+                  <button onClick={() => setIsModalOpen(true)}>
+                    구매 상담 신청
+                  </button>
+                </div>
+
+                {isModalOpen && (
+                  <Proposal onClose={() => setIsModalOpen(false)} />
+                )}
+              </div>
+            </div>
+          </div>
+        );
+      case 'consultation':
+        return <p>내 상담 내역이 표시됩니다.</p>;
+      case 'profile':
+        return <p>회원 정보 수정 페이지입니다.</p>;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className="my-page">
+      <div className="button-group">
+        <button
+          onClick={() => setActivePage('interest')}
+          className={activePage === 'interest' ? 'active' : ''}
+        >
+          나의 관심 차량
+        </button>
+        <button
+          onClick={() => setActivePage('consultation')}
+          className={activePage === 'consultation' ? 'active' : ''}
+        >
+          내 상담 내역
+        </button>
+        <button
+          onClick={() => setActivePage('profile')}
+          className={activePage === 'profile' ? 'active' : ''}
+        >
+          회원 정보 수정
+        </button>
+      </div>
+      <div className="content-area">{renderContent()}</div>
+    </div>
+  );
+}
+
+export default MyPage;
