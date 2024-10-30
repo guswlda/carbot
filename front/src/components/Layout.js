0e2d15ff64101941bca8e56f9822356b82c4f6c1
cm <mingyeong775@naver.com>
2024-10-30 11:31:42 +0900

    f_page_fixed

--- front/src/components/Layout.js
+++ front/src/components/Layout.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '../components/header/header'; // Header 컴포넌트 import
 import Footer from '../components/footer/footer'; // Footer 컴포넌트 import
 import { Outlet } from 'react-router-dom';
+import './Layout.css';
 
 const Layout = () => {
   return (
